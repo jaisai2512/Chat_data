5eb7afbff38eb63854be5c727cf80b81db4d86a3
jaisai2512 <104774963+jaisai2512@users.noreply.github.com>
2024-12-30 16:45:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -298,6 +298,7 @@ if uploaded_file is not None:
     </div>
 """
             except:
+                    st.write('er')
                     div = f"""
                 <div class="chat-row" style="background-color: {background_color}; padding: 10px; margin: 5px; border-radius: 5px;">
                     <strong>{label}:</strong>
